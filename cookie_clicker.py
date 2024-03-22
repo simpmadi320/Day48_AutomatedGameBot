5a4a74f4e3650938b5cce3ca79b7d05041d626b8
Madison Simpson <madison_simpson@outlook.com>
2024-03-21 23:18:08 -0400

    Technically finished, but I want to do more!

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -3,6 +3,16 @@ from selenium.webdriver.common.by import By
 
 import time
 
+
+def get_upgrades(cookie_driver):
+    all_buttons = cookie_driver.find_elements(By.CSS_SELECTOR, value="#store b")
+    to_return = {}
+    for n in range(len(all_buttons) - 1):
+        text = all_buttons[n].text.split("-")
+        to_return[n] = {"Clickable": all_buttons[n], "Item": text[0].strip(),
+                       "Cost": int(text[1].strip().replace(",", ""))}
+    return to_return
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
@@ -11,14 +21,7 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, value="cookie")
 
-all_clickables = driver.find_elements(By.CSS_SELECTOR, value="#store b")
-
-upgrades = {}
-for n in range(len(all_clickables) - 1):
-    text = all_clickables[n].text.split("-")
-    upgrades[n] = {"Clickable": all_clickables[n], "Item": text[0].strip(), "Cost": int(text[1].strip().replace(",", ""))}
-
-print(upgrades)
+upgrades = get_upgrades(driver)
 
 timeout = time.time() + 1
 cookies = 0
@@ -26,7 +29,7 @@ cookies = 0
 while True:
 
     if time.time() > timeout:
-        timeout = time.time() + 0.01
+        timeout = time.time() + 0.01a
         cookie.click()
         cookies += 1
 
@@ -34,4 +37,6 @@ while True:
             upgrade = upgrades[upgrade_key]
             if cookies > upgrade["Cost"]:
                 print("You can buy a " + upgrade["Item"] + " as you have " + str(cookies) + " cookies")
-                
+                cookies -= upgrade["Cost"]
+                upgrade["Clickable"].click()
+                upgrades = get_upgrades(driver)
