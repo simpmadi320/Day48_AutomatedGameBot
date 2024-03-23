83d27fc5f2da74fcdf076dad2d7b84e78a785ac5
Madison Simpson <madison_simpson@outlook.com>
2024-03-22 21:17:15 -0400

    Technically finished, but I want to do more!

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 
+def Get_Clickables():
+    all_buttons = driver.find_elements(By.CSS_SELECTOR, value="#store b")
 
-def get_upgrades(cookie_driver):
-    all_buttons = cookie_driver.find_elements(By.CSS_SELECTOR, value="#store b")
-    to_return = {}
+    cookie_upgrades = {}
     for n in range(len(all_buttons) - 1):
         text = all_buttons[n].text.split("-")
-        to_return[n] = {"Clickable": all_buttons[n], "Item": text[0].strip(),
-                       "Cost": int(text[1].strip().replace(",", ""))}
-    return to_return
+        cookie_upgrades[n] = {"Clickable": all_buttons[n], "Item": text[0].strip(),
+                              "Cost": int(text[1].strip().replace(",", ""))}
+    return cookie_upgrades
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -21,22 +21,27 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, value="cookie")
 
-upgrades = get_upgrades(driver)
-
 timeout = time.time() + 1
 cookies = 0
 
 while True:
 
     if time.time() > timeout:
-        timeout = time.time() + 0.01a
+        timeout = time.time() + 0.01
         cookie.click()
         cookies += 1
 
+        try:
+            upgrades = Get_Clickables()
+        except StaleElementReferenceException:
+            continue
+
+
         for upgrade_key in upgrades:
             upgrade = upgrades[upgrade_key]
-            if cookies > upgrade["Cost"]:
-                print("You can buy a " + upgrade["Item"] + " as you have " + str(cookies) + " cookies")
-                cookies -= upgrade["Cost"]
-                upgrade["Clickable"].click()
-                upgrades = get_upgrades(driver)
+            try:
+                if cookies > upgrade["Cost"]:
+                    print("You can buy a " + upgrade["Item"] + " as you have " + str(cookies) + " cookies")
+                    upgrade["Clickable"].click()
+            except StaleElementReferenceException:
+                continue
